a6cf8ad65e5d18401b0a52691eedf03bdda783c1
Ishita Kabra <ishitakabra1805@gmail.com>
2021-01-05 17:57:29 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -3,27 +3,29 @@ const express = require('express')
 const app = express()
 const port =  process.env.PORT || 3001
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const mongoose = require('mongoose')
 require('./db/mongoose.js')
 mongoose.set('useCreateIndex',true);
 
 // middleware
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Set-Cookie,Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Set-Cookie,Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 
 // Routes

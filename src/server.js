29590548a289e37772e11a65d491c8376a9741fa
Ishita Kabra <ishitakabra1805@gmail.com>
2021-02-19 19:09:22 +0530

    cors

--- src/server.js
+++ src/server.js
@@ -17,19 +17,6 @@ app.use(fileUpload({
   useTempFiles:true
 }));
 app.use(cors())
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://heed-india.herokuapp.com");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Set-Cookie,Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Credentials", true);
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
 
 
 // Routes

2dfb4ab5971b8a1d112b2c1151d8541de8700a45
Ishita Kabra <ishitakabra1805@gmail.com>
2021-02-19 16:06:53 +0530

    url change

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(fileUpload({
   useTempFiles:true
 }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://heed-india.herokuapp.com/");
   res.header(
     "Access-Control-Allow-Headers",
     "Set-Cookie,Origin, X-Requested-With, Content-Type, Accept"

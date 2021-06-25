e7e542f38b54997394b374ea112c7aaafa890505
milanmandal <milanmandal2001@gmail.com>
2021-06-26 02:59:17 +0530

    email

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -2,11 +2,15 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const email = (data) => {
+  console.log(data);
   const msg = {
-    to: `${data.email}`,
+    to: 'milan.mandal2019@vitstudent.ac.in',
     from: 'milanmandal2001@gmail.com',
-    subject: 'Sending with SendGrid is Fun',
-    html: '<strong></strong>',
+    dynamic_template_data: {
+      "name": `${data.name}`,
+      "amt" :`${data.amt}`,
+    },
+    template_id:'d-7590aa394d6140b3aa5f845e76c8963e'
   };
   sgMail.send(msg)
   .then(() => {

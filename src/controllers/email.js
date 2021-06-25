f28db7d76dd3528aeecca38d539a5cba2508b72c
Ishita Kabra <ishitakabra1805@gmail.com>
2021-06-25 20:16:52 +0530

    bug fixes

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -1,17 +1,20 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-
-function email(data)
-{
+const email = (data) => {
   const msg = {
     to: `${data.email}`,
-    from: 'test@gmail.com',
+    from: 'milanmandal2001@gmail.com',
     subject: 'Sending with SendGrid is Fun',
-    text: `Name: ${data.name}\nPhone No: ${data.contact}`,
     html: '<strong></strong>',
   };
-  sgMail.send(msg);
+  sgMail.send(msg)
+  .then(() => {
+    console.log('done')
+  })
+  .catch((e) => {
+    console.log(e);
+  })
 }
 
 module.exports = email;
